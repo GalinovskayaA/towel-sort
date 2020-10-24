dfa5f7dcba1ad63ad366ba9a40d8fa1a6295f253
GalinovskayaA <galiale@bk.ru>
2020-10-24 19:41:48 +0300

    feat: make function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let nArr = [];
+  if (matrix === undefined) return []
+  for (let i=0; i < matrix.length; i++)
+  if (i % 2 === 0) {
+    nArr.push(...matrix[i]);
+  } else {
+    let revArr = matrix[i].reverse()
+    nArr.push(...revArr);
+  } 
+  return nArr
 }
